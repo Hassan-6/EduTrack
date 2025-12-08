fc33a0f96552f69b45dd7e4ff06081bc6d675ad6
Raiden216 <oneebtariq@gmail.com>
2025-12-09 00:40:48 +0500

    Minor fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.edutrack"
+    namespace = "com.raiden216.edutrack"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.edutrack"
+        applicationId = "com.raiden216.edutrack"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
